cb1d26f8b2dca7a3b5062da019d9524344941429
Giriiri-g <giri090205@gmail.com>
2024-04-23 15:02:24 +0530

    update

--- 22AIE214 - Robotics/lab1.m
+++ 22AIE214 - Robotics/lab1.m
@@ -8,16 +8,17 @@ show(atlas);
 show(ur10);
 
 %Generating joint configurations
-for i =1:4
-	subplot(2,2,i);
-	config = randomConfiguration(gen3)
-	show(gen3,config);
+for i =1:9
+	subplot(3,3,i);
+	config = randomConfiguration(ur10);
+	show(ur10,config);
 end
 
 %Interacting with Robot model
-interactiveGUI = interactiveRigidBodyTree(gen3);
+interactiveGUI = interactiveRigidBodyTree(ur10);
+
 
 %Saving the joint configuration
-interactiveGUI.Configuration = randomConfiguration(gen3);
+interactiveGUI.Configuration = randomConfiguration(ur10);
 addConfiguration(interactiveGUI)
 disp(interactiveGUI.StoredConfigurations)
