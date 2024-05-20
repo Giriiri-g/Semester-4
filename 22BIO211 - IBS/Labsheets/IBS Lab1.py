1ef2e63aa734a98f5dac5aa07623295d94370b4d
Giriiri-g <giri090205@gmail.com>
2024-05-20 09:24:08 +0530

    IBS Labsheets

--- 22BIO211 - IBS/Labsheets/IBS Lab1.py
+++ 22BIO211 - IBS/Labsheets/IBS Lab1.py
@@ -8,3 +8,33 @@ graph = { "a": ["c"],
           "f": []
           }
 
+print(graph)
+
+print('\n\n\n')
+
+print(graph.keys())
+
+print('\n\n\n')
+
+edges = []
+print("Edges:")
+for i in graph:
+     for j in graph[i]:
+          if {i, j} not in edges:
+               edges.append({i, j})
+               print(f"{i} -> {j}")
+
+
+## [q2]
+
+graph = {}
+
+while True:
+     edge = input("Enter an Edge(a, b) or QUIT: ")
+     try:
+          edge = edge.replace(' ', '')
+          if edge == 'QUIT':
+               break
+          vertex1 = edge[1].upper()
+          vertex2 = edge[3].upper()
+          graph[vertex1] = vertex2
